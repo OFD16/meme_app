1b383c023f421100746826015fbca101f46d18e0
OFD16 <96663769+OFD16@users.noreply.github.com>
2022-10-02 20:41:51 +0300

    sounplayer colors fixed

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import SignUpScreen from './src/screens/SignUpScreen';
 //Abdulkadir
 import ProfileScreen from './src/screens/ProfileScreen';
 import EditProfileScreen from './src/screens/EditProfileScreen';
-//import { View, Text, ScrollView, Button, StyleSheet} from 'react-native';
+import { View, Text, ScrollView, Button, StyleSheet} from 'react-native';
 import TextButton from './src/components/buttons/TextButton';
 import OutlinedButton from './src/components/buttons/OutlinedButton';
 import FilledButton from './src/components/buttons/FilledButton';
@@ -22,20 +22,6 @@ import CustomTextInput from './src/components/inputs/CustomTextInput';
 import { useState } from 'react';
 import CircleSoundCard from './src/components/cards/CircleSoundCard';
 import SquareSoundCard from './src/components/cards/SquareSoundCard';
-
-import * as React from 'react';
-import { Audio } from 'expo-av';
-
-import {
-  ScrollView,
-  Text,
-  View,
-  StyleSheet,
-  ActivityIndicator,
-  Button,
-} from 'react-native';
-import Constants from 'expo-constants';
-
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -51,113 +37,42 @@ export default function App() {
 
 function Test() {
 
-  const SampleTrack = require('./assets/sütbalyumurta.mp3');
+  const [passVisibility, setPassVisibility] = useState(true);
+  function changeVisibility() {
+    setPassVisibility(!passVisibility);
+  };
 
+  const [passVisibility2, setPassVisibility2] = useState(true);
+  function changeVisibility2() {
+    setPassVisibility2(!passVisibility2);
+  };
+
+  return (
+    <ScrollView contentContainerStyle={{
+      padding: 16,
+      alignItems: "center",
+    }}>
+      <TextButton onPress={() => {}} text="Hello World"/>
+      <OutlinedButton onPress={() => {}} text="Hello World"/>
+      <FilledButton onPress={() => {}} text="Hello World"/>
+      <IconButton onPress={() => {}} iconName="play"/>
+      <TextButtonIcon onPress={() => {}} iconName="play" text="Hello World"/>
+      <OutlinedButtonIcon onPress={() => {}} iconName="play" text="Hello World"/>
+      <FilledButtonIcon onPress={() => {}} iconName="play" text="Hello World"/>
+
+      <CustomTextInput placeholder="Email" isLeading={true} leadingIconName="mail-outline" keyboardType="email-address"/>
+      <CustomTextInput placeholder="Telefon numarası" isLeading={true} leadingIconName="call-outline" keyboardType="phone-pad"/>
+      <CustomTextInput placeholder="Sadece sayılar" isLeading={true} leadingIconName="podium-outline" keyboardType="numeric"/>
+      <CustomTextInput placeholder="Şifre" isTrailing={true} isPassword={true} isVisible={passVisibility} onPress={changeVisibility}/>
+      <CustomTextInput placeholder="Şifre" isLeading={true} leadingIconName="lock-closed-outline" isTrailing={true} isPassword={true} isVisible={passVisibility2} onPress={changeVisibility2}/>
+      <CustomTextInput placeholder="Meme ara..." isTrailing={true} trailingIconName="search-outline" keyboardType="web-search"/>
+
+      <CircleSoundCard source={require("./assets/sound-background.jpg")} duration="2:40"/>
+      <SquareSoundCard source={require("./assets/sound-background.jpg")} duration="2:40" title="Togg Otomobilimiz Yerli ve Milli"/>
+    </ScrollView>
+  );
   
-    const [Loaded, SetLoaded] = React.useState(false);
-    const [Loading, SetLoading] = React.useState(false);
-    const sound = React.useRef(new Audio.Sound());
   
-    React.useEffect(() => {
-      LoadAudio();
-    }, []);
-  
-    const PlayAudio = async () => {
-      try {
-        const result = await sound.current.getStatusAsync();
-        if (result.isLoaded) {
-          if (result.isPlaying === false) {
-            sound.current.playAsync();
-            console.log('started');
-          }
-        }
-      } catch (error) {}
-    };
-  
-    const PauseAudio = async () => {
-      try {
-        const result = await sound.current.getStatusAsync();
-        if (result.isLoaded) {
-          if (result.isPlaying === true) {
-            sound.current.pauseAsync();
-            console.log('paused');
-          }
-        }
-      } catch (error) {}
-    };
-    const LoadAudio = async () => {
-      SetLoading(true);
-      const checkLoading = await sound.current.getStatusAsync();
-      if (checkLoading.isLoaded === false) {
-        try {
-          const result = await sound.current.loadAsync(SampleTrack, {}, true);
-          if (result.isLoaded === false) {
-            SetLoading(false);
-            console.log('Error in Loading Audio');
-          } else {
-            SetLoading(false);
-            SetLoaded(true);
-          }
-        } catch (error) {
-          console.log(error);
-          SetLoading(false);
-        }
-      } else {
-        SetLoading(false);
-      }
-    };
-
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#ecf0f1',
-        padding: 8,
-      },
-      AudioPLayer: {
-        width: '100%',
-        height: 50,
-        alignItems: 'center',
-      },
-    });
-  
-    return (
-      <View style={styles.container}>
-        <View style={styles.AudioPLayer}>
-          {Loading ? (
-            <ActivityIndicator size={'small'} color={'red'} />
-          ) : (
-            <>
-              {Loaded === false ? (
-                <>
-                  <ActivityIndicator />
-                  <Text>Loading Song</Text>
-                </>
-              ) : (
-                <>
-                  <Button title="Play Song" onPress={PlayAudio} />
-                  <Button title="Pause Song" onPress={PauseAudio} />
-                </>
-              )}
-            </>
-          )}
-        </View>
-      </View>
-    );
-  
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -198,46 +113,5 @@ function Test() {
   //     <View style={{marginTop: 10}}></View>
   //     <Button title="pause Sound" onPress={pauseSound} />
   //   </View>
-  // );
-
-
-
-
-
-
-
-  // const [passVisibility, setPassVisibility] = useState(true);
-  // function changeVisibility() {
-  //   setPassVisibility(!passVisibility);
-  // };
-
-  // const [passVisibility2, setPassVisibility2] = useState(true);
-  // function changeVisibility2() {
-  //   setPassVisibility2(!passVisibility2);
-  // };
-
-  // return (
-  //   <ScrollView contentContainerStyle={{
-  //     padding: 16,
-  //     alignItems: "center",
-  //   }}>
-  //     <TextButton onPress={() => {}} text="Hello World"/>
-  //     <OutlinedButton onPress={() => {}} text="Hello World"/>
-  //     <FilledButton onPress={() => {}} text="Hello World"/>
-  //     <IconButton onPress={() => {}} iconName="play"/>
-  //     <TextButtonIcon onPress={() => {}} iconName="play" text="Hello World"/>
-  //     <OutlinedButtonIcon onPress={() => {}} iconName="play" text="Hello World"/>
-  //     <FilledButtonIcon onPress={() => {}} iconName="play" text="Hello World"/>
-
-  //     <CustomTextInput placeholder="Email" isLeading={true} leadingIconName="mail-outline" keyboardType="email-address"/>
-  //     <CustomTextInput placeholder="Telefon numarası" isLeading={true} leadingIconName="call-outline" keyboardType="phone-pad"/>
-  //     <CustomTextInput placeholder="Sadece sayılar" isLeading={true} leadingIconName="podium-outline" keyboardType="numeric"/>
-  //     <CustomTextInput placeholder="Şifre" isTrailing={true} isPassword={true} isVisible={passVisibility} onPress={changeVisibility}/>
-  //     <CustomTextInput placeholder="Şifre" isLeading={true} leadingIconName="lock-closed-outline" isTrailing={true} isPassword={true} isVisible={passVisibility2} onPress={changeVisibility2}/>
-  //     <CustomTextInput placeholder="Meme ara..." isTrailing={true} trailingIconName="search-outline" keyboardType="web-search"/>
-
-  //     <CircleSoundCard source={require("./assets/sound-background.jpg")} duration="2:40"/>
-  //     <SquareSoundCard source={require("./assets/sound-background.jpg")} duration="2:40" title="Togg Otomobilimiz Yerli ve Milli"/>
-  //   </ScrollView>
   // );
 }
--- src/components/cards/CircleSoundCard.js
+++ src/components/cards/CircleSoundCard.js
@@ -1,20 +1,104 @@
-import { Text, View, ImageBackground, StyleSheet } from 'react-native';
+import { Text, View, ImageBackground, StyleSheet, Pressable, ActivityIndicator, Button } from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons";
 import { appColors } from '../../constants';
 
+import * as React from 'react';
+import { Audio } from 'expo-av';
+
 export default function CircleSoundCard({ source, duration }) {
+    const SampleTrack = require('../..//../assets/sütbalyumurta.mp3');
+
+    const [Loaded, SetLoaded] = React.useState(false);
+    const [Loading, SetLoading] = React.useState(false);
+    const [Playing, SetPlaying] = React.useState(false);
+    const sound = React.useRef(new Audio.Sound());
+
+    React.useEffect(() => {
+        LoadAudio();
+    }, []);
+
+    const PlayAudio = async () => {
+        try {
+            const result = await sound.current.getStatusAsync();
+            if (result.isLoaded) {
+                if (result.isPlaying === false) {
+                    sound.current.playAsync();
+                    console.log('started');
+                    SetPlaying(true);
+                }
+                console.log(result.isPlaying);
+            }
+        } catch (error) { }
+    };
+
+    const PauseAudio = async () => {
+        try {
+            const result = await sound.current.getStatusAsync();
+            if (result.isLoaded) {
+                if (result.isPlaying === true) {
+                    sound.current.pauseAsync();
+                    console.log('paused');
+                    SetPlaying(false);
+                }
+                console.log(result.isPlaying);
+            }
+        } catch (error) { }
+    };
+    const LoadAudio = async () => {
+        SetLoading(true);
+        const checkLoading = await sound.current.getStatusAsync();
+        if (checkLoading.isLoaded === false) {
+            try {
+                const result = await sound.current.loadAsync(SampleTrack, {}, true);
+                if (result.isLoaded === false) {
+                    SetLoading(false);
+                    console.log('Error in Loading Audio');
+                } else {
+                    SetLoading(false);
+                    SetLoaded(true);
+                }
+            } catch (error) {
+                console.log(error);
+                SetLoading(false);
+            }
+        } else {
+            SetLoading(false);
+        }
+    };
+
     return (
-        <ImageBackground
-            style={circleSoundCardStyles.circleSoundCard}
-            imageStyle={{borderRadius: 40, borderWidth: 2, borderColor: appColors.secondaryColor}}
-            source={source}>
-            <View style={circleSoundCardStyles.circleSoundCardInsider}>
-                <Icon color={appColors.primaryColor} name="play" size={24}/>
-                <Text style={circleSoundCardStyles.circleSoundCardDuration}>{duration}</Text>
+            <View>
+                {Loading ? (
+                    <ActivityIndicator size={'small'} color={'red'} />
+                ) : (
+                    <>
+                        {Loaded === false ? (
+                            <>
+                                <ActivityIndicator />
+                                <Text>Loading Song</Text>
+                            </>
+                        ) : (
+                            <>
+                                <Pressable onPress={Playing ? PauseAudio : PlayAudio}>
+                                    <ImageBackground
+                                        style={circleSoundCardStyles.circleSoundCard}
+                                        imageStyle={{ borderRadius: 40, borderWidth: 2, borderColor: appColors.secondaryColor }}
+                                        source={source}>
+                                        <View style={circleSoundCardStyles.circleSoundCardInsider}>
+                                            <Icon color={Playing ? "#54bfff" : appColors.primaryColor} name= {Playing ? "pause" : "play"} size={24} />
+                                            <Text style={Playing ? circleSoundCardStyles.circleSoundCardDurationActive : circleSoundCardStyles.circleSoundCardDuration}>{duration}</Text>
+                                        </View>
+                                    </ImageBackground>
+                                </Pressable>
+                            </>
+                        )}
+                    </>
+                )}
             </View>
-        </ImageBackground>
     );
 }
+
+
 
 const circleSoundCardStyles = StyleSheet.create({
     circleSoundCard: {
@@ -35,6 +119,19 @@ const circleSoundCardStyles = StyleSheet.create({
         bottom: -8,
         alignSelf: "center",
         backgroundColor: appColors.greyColor,
+        fontSize: 12,
+        paddingVertical: 2,
+        paddingHorizontal: 4,
+        borderRadius: 4,
+        borderColor: appColors.primaryColor,
+        borderWidth: 1,
+    },
+    circleSoundCardDurationActive: {
+        color: appColors.primaryColor,
+        position: "absolute",
+        bottom: -8,
+        alignSelf: "center",
+        backgroundColor: "#54bfff",
         fontSize: 12,
         paddingVertical: 2,
         paddingHorizontal: 4,
